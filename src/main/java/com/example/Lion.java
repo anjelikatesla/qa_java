4afc38a40d662321128cbb4397f4a832243b9b86
anjelikatesla <anjelika.tesla@yandex.ru>
2022-10-09 19:18:26 +0600

    Sprint-6 Исправлен Lion.java, добавлен pom.xml.

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -6,7 +6,7 @@ public class Lion {
 
     boolean hasMane;
 
-    private Feline feline;
+    private final Feline feline;
 
     public Lion(String sex, Feline feline) throws Exception {
         this.feline = feline;
